81b2047ea92197a6d88f50d078ed01c7c3fac17c
jay45652 <sgcheon@naver.com>
2023-04-24 01:16:00 +0900

    2023-04-23

--- LevelTest_starter/src/Test2.java
+++ LevelTest_starter/src/Test2.java
@@ -1,6 +1,6 @@
 public class Test2 {
     public String solution(String phone_number) {
-        String answer = "";
+
 
         String[] str = phone_number.split("");
         for (int i = 0; i < phone_number.length() - 4; i++) {
